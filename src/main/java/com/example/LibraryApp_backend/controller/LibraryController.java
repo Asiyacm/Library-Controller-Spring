59eff0ee440ec5df22e3fb25f5765e7354a7c084
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-05 21:01:43 +0530

    adminLogin commit

--- src/main/java/com/example/LibraryApp_backend/controller/LibraryController.java
+++ src/main/java/com/example/LibraryApp_backend/controller/LibraryController.java
@@ -1,10 +1,22 @@
 package com.example.LibraryApp_backend.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 
 public class LibraryController {
+
+
+    @GetMapping("/")
+
+    public String Adminpage(){
+
+        return("Welcome to Library Adminpage");
+
+    }
+
+
 }
